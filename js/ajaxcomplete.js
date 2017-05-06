bfb2e86fc48904b62c3c08b021efb94dcf07ab7a
Teague Hopkins <teague@teaguehopkins.com>
2017-05-06 14:25:10 -0400

    Missing semicolons and comparators

--- js/ajaxcomplete.js
+++ js/ajaxcomplete.js
@@ -3,9 +3,9 @@ function callSearch() {
   var searchText = document.getElementById("ajax-example").value;
   ajax.open("GET", "http://localhost:3000/search?query="+searchText, true);
   ajax.onload = function() {
-  	if(searchText != '') {
+  	if(searchText !== '') {
       var questions = JSON.parse(ajax.responseText).map(function(i) { return i.questionEn; });
-      var answers = JSON.parse(ajax.responseText).map(function(i) { return i.answerEn });
+      var answers = JSON.parse(ajax.responseText).map(function(i) { return i.answerEn; });
       var section = document.getElementById('questions');
       section.innerHTML = '';
       for(i=0; i < questions.length; i++) {
